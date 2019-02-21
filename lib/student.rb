482ba9b4aafd1fedd71e5f28d045fceeb495d3dd
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-21 18:39:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,12 @@ class Student < User
   def initialize()
       @knowledge = []
   end
+  
+  def learn(knowledge)
+      @knowledge << knowledge
+  end
 
+  def knowledge
+    return @knowledge
+  end
 end
